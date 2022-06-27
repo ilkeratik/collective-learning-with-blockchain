ac411d505fbf2b0e613ceb521f704e4938d00cd3
ilkeratik <ilkeratik35@gmail.com>
2022-06-27 17:10:47 +0300

    Add wallet and transaction & update server app

--- backend/tests/util/test_hex_to_binary.py
+++
@@ -1,9 +0,0 @@
-from backend.bc_utils.hex_to_binary import hex_to_binary
-
-
-def test_hex_to_binary():
-    org_num = 908
-    hex_num = hex(org_num)
-    binary_num = hex_to_binary(hex_num)
-
-    assert int(binary_num,2) == org_num
